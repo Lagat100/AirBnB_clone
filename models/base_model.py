48d080d3573d57e51aa8771435e320351ce7f229
rayoo22 <ryankibet14@gmail.com>
2024-02-05 21:42:55 -0800

    recreates an instance using dict representation

--- models/base_model.py
+++ models/base_model.py
@@ -1,18 +1,48 @@
 #!/usr/bin/python3
 """BaseModel module"""
-import uuid
-import datetime
+from uuid import uuid4
+from datetime import datetime
 
 
 class BaseModel:
     """BaseModel class - template for all other classes"""
-    def __init__(self, id, created_at, updated_at):
-        self.id = string(uuid.uuid4())
-        self.created_at = datetime.datetime.now()
-        self.updated_at = datetime.datetime.now()
+    def __init__(self, *args, **kwargs):
+        """Initialize attributes: random uuid, dates created/updated
+        """
+        if kwargs:
+            for key, val, in kwargs.items():
+                if "created_at" == key:
+                    self.created_at = datetime.strptime(kwargs["created_at"],
+                        "%Y-%m-%dT%H:%M:%S.%f")
+                elif "updated_at" == key:
+                    self.updated_at = datetime.strptime(kwargs["updated_at"],
+                        "%Y-%m-%dT%H:%M:%S.%f")
+                elif "__class__" == key:
+                    pass
+                else:
+                    setattr(self, key, val)
+        
+        else:
+            self.id = str(uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
-        return f"[BaseModel] ({self.id}) {<self.__dict__>}"
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
+        """Updates instance 'updated_at' with updated time and saves to a serialized file
+        """
+        self.updated_at = datetime.now()
 
+    def to_dict(self):
+        """Return dict with string formats of times;add class info to dict
+        """
+        dict = {}
+        dict["__class__"] = self.__class__.__name__
+        for k, v in self.__dict__.items():
+            if isinstance(v, (datetime, )):
+                dict[k] = v.isoformat()
+            else:
+                dict[k] = v
+        return dict
